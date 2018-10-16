891bd75c67bd1aec5dedd1e366094476dc5610b5
Hana Hasan <hahasan@stu.naperville203.org>
2018-10-16 12:01:02 -0500

    I added a trash class to practice commiting to repo in RasPi

---
+++ LinkedList/trashClass.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class trashClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class trashClass
+{
+    public static void main(String [] args) 
+    {
+        System.out.print("Ya trashy yeet!"); 
+    }
+}
